f51fe6f75d185e22c3ea2dc86f868cd5cb2c70f9
walter-medina <medinawalter3@gmail.com>
2025-09-10 20:53:22 -0500

    solution error gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.clase3"
-    compileSdk = 33
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.clase3"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
