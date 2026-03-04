3c0262c2640cf7ca724dc1d67426e582b7523a90
waltermedinacode <medinawalter3@gmail.com>
2026-03-03 21:13:50 -0500

    Remove build folder from tracking

--- app/build/generated/ap_generated_sources/debug/out/com/example/clase3/BR.java
+++
@@ -1,5 +0,0 @@
-package com.example.clase3;
-
-public class BR {
-  public static final int _all = 0;
-}
--- app/build/generated/data_binding_trigger/debug/com/example/clase3/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.clase3;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
